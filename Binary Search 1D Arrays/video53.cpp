0289451de60b1f3212375707e6ad7220917ee5fa
Vidit <viditgupta1126@gmail.com>
2024-06-25 18:27:50 +0530

    25/06/24

--- Binary Search 1D Arrays/video53.cpp
+++ Binary Search 1D Arrays/video53.cpp
@@ -18,18 +18,13 @@ int peakElementApproach2(vector<int>a){
     //element at left and right should be smaller than mid element
     int n=a.size();
     if(n==1) return 0;
+    if(a[0]>a[1]) return 0;
+    if(a[n-1]>a[n-2]) return n-1;
 
     int low=1;
     int high=n-2;
     while(low<=high){
-        int mid=(low+high)/2;
-        if(mid==0) {
-            if(a[0]>a[1]) return 0;
-        }
-        if(mid==n-1) {
-            if(a[n-1]>a[n-2]) return n-1;
-        }
-        
+        int mid=(low+high)/2;        
         if(a[mid]>a[mid-1] && a[mid]>a[mid+1]){
             return mid; 
         }
