51ec317073374ca467a2a2fe79322d0a7a141763
Vidit <viditgupta1126@gmail.com>
2024-06-27 23:29:11 +0530

    27/06/24

--- Strings/151. Reverse Words in a String.cpp
+++ Strings/151. Reverse Words in a String.cpp
@@ -1,20 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string trim(string s){
+    string trim(string s){
         int i=0;
         int j=s.length()-1;
         while(s[i]==' '&& i<j) i++;
         while(s[j]==' '&& i<j) j--;
-        return s.substr(i,j+1);
+        return s.substr(i,j-i+1);
     }
+    string reverseWord(string str){
+        int x=0;
+        int y=0;
+        string s=str;
+        for(int i=0;i<=s.length();i++){
+            if(s[i]==' ' || i==s.length()){
+                y=i;
+                reverse(s.begin()+x,s.begin()+y);
+                x=i+1;
+            }
+        }
+        return s;
+    }
+
     string reverseWords(string s) {
-        string str=trim(s);
-        cout<<str;
+        s=trim(s);
+        s=reverseWord(s);
+        reverse(s.begin(),s.end());
+        return s;
     }
 
 int main(){
-    string s="the sky is blue";
-    reverseWords(s);
+    string s="    the sky is blue    ";
+    cout<<reverseWords(s);
     return 0;
 }
