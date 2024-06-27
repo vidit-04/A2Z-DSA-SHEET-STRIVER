cc025eb83240ed061a21a01a131c339b6b6f673d
Vidit <viditgupta1126@gmail.com>
2024-06-28 00:46:29 +0530

    27/06/24

--- Strings/151. Reverse Words in a String.cpp
+++ Strings/151. Reverse Words in a String.cpp
@@ -8,29 +8,49 @@ using namespace std;
         while(s[j]==' '&& i<j) j--;
         return s.substr(i,j-i+1);
     }
-    string reverseWord(string str){
-        int x=0;
-        int y=0;
-        string s=str;
-        for(int i=0;i<=s.length();i++){
-            if(s[i]==' ' || i==s.length()){
-                y=i;
-                reverse(s.begin()+x,s.begin()+y);
-                x=i+1;
-            }
+    // string reverseWordOfString(string str){
+    //     int x=0;
+    //     int y=0;
+    //     string s=str;
+    //     for(int i=0;i<=s.length();i++){
+    //         if(s[i]==' ' || i==s.length()){
+    //             y=i;
+    //             reverse(s.begin()+x,s.begin()+y);
+    //             x=i+1;
+    //         }
+    //     }
+    //     return s;
+    // }
+
+    vector<string> split(string s){
+        stringstream st(s); // Used to create input stream so that you can input string
+        vector<string>str;
+        string token;
+        char delimeter=' ';
+        // while(getline(st,token,delimeter)){
+        while(st>>token){
+            if(!token.empty()) str.push_back(token);
         }
-        return s;
+        return str;
+    }
+    string join(vector<string>s){
+        string str="";
+        for(int i=0;i<s.size();i++){
+            str+=s[i];
+            if(i!=s.size()-1) str+=" ";
+        }
+        return str;
     }
 
     string reverseWords(string s) {
-        s=trim(s);
-        s=reverseWord(s);
-        reverse(s.begin(),s.end());
+        vector<string>str=split(s);
+        reverse(str.begin(),str.end());
+        s=join(str);
         return s;
     }
 
 int main(){
-    string s="    the sky is blue    ";
+    string s="the sky       is blue";
     cout<<reverseWords(s);
     return 0;
 }
