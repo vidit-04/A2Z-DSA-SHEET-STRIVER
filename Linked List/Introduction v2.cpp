78e07c969c0580564ee49fce8dc2b6a7bb96bd60
Vidit <viditgupta1126@gmail.com>
2024-07-01 23:16:48 +0530

    01/07/24

--- Linked List/Introduction v2.cpp
+++ Linked List/Introduction v2.cpp
@@ -18,9 +18,56 @@ class Node{
 
 };
 
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp; 
+        mover = temp;  // mover=mover->next
+    }
+    return head;
+}
+
+void printingLL(Node* head){
+    Node*temp=head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+int LengthOfLL(Node* head){
+    int len=0;
+    Node* temp=head;
+    while(temp){
+        len++;
+        temp=temp->next;
+    }
+    return len;
+}
+
+bool SearchElementInLL(Node* head, int target){
+    Node* temp=head;
+    while(temp!=nullptr){
+        if(temp->data==target) return true;
+        temp=temp->next;
+    }
+    return false;
+}
+
 int main(){
-    vector<int>arr={2,5,7,8};
-    Node* y= new Node(arr[0],nullptr);
-    cout<<y->data;
+    vector<int>arr={12,5,7,8};
+    // Node* y= new Node(arr[0],nullptr);
+    // cout<<y;
+    // cout<<y->data;
+
+    Node* head= convertArr2LL(arr);
+    // cout<<head->data;
+    printingLL(head);
+    cout<<endl;
+    cout<<LengthOfLL(head);
+    cout<<endl;
+    cout<<SearchElementInLL(head, 7);    
     return 0;
 }
