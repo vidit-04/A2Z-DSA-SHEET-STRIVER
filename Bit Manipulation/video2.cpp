0f221b1330fd8683f68e5559a49b8c910ecf88d2
Vidit <viditgupta1126@gmail.com>
2024-07-14 17:21:21 +0530

    14/07/24

--- Bit Manipulation/video2.cpp
+++ Bit Manipulation/video2.cpp
@@ -34,11 +34,37 @@ int setIthBit(int n,int i){
 
 }
 
+int clearIthBit(int n,int i){
+    //USING LEFT SHIFT OPERATOR
+    return (n & ~(1<<i));
+}
+
+int ToggleIthBit(int n,int i){
+    //USING LEFT SHIFT OPERATOR
+    return (n ^ 1<<i);
+}
+
+int RemoveFirstSetBitFromRight(int n){
+    //in power of two we have only 1 up bit.
+    //if we subtract 1 from no. then resultant number will have that bit as 0 and rest bits as 1.
+    //if we do and operation of n and n-1 then it will be 0.
+    //FOR ex- 16- 1000 and 15-0111 then after & it is equal to 0 thus ans.
+    return (n & (n-1));
+}
+
+int checkIfNisPowerOf2(int n){
+    //in power of two we have only 1 up bit.
+    //if we subtract 1 from no. then resultant number will have that bit as 0 and rest bits as 1.
+    //if we do and operation of n and n-1 then it will be 0.
+    //FOR ex- 16- 1000 and 15-0111 then after & it is equal to 0 thus ans.
+    return (n & (n-1))==0;
+}
+
 int main(){
-    // int a=3;
-    // int b=2;
-    // swapApproach2(a,b);
-    // cout<<a<<" "<<b;
+    int a=3;
+    int b=2;
+    swapApproach2(a,b);
+    cout<<a<<" "<<b<<endl;
 
     int n;
     cin>>n;
@@ -46,5 +72,10 @@ int main(){
     cin>>i;
     cout<<checkIfIthBitIsSetApproach2(n,i)<<endl;
     cout<<setIthBit(n,i)<<endl;
+    cout<<clearIthBit(n,i)<<endl;
+    cout<<ToggleIthBit(n,i)<<endl;
+    cout<<RemoveFirstSetBitFromRight(n)<<endl;
+    cout<<checkIfNisPowerOf2(n)<<endl;
+
     return 0;
 }
