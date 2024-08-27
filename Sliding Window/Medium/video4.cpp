6e3d6f938e24ceb69185541e2333dab1b7d45823
Vidit <viditgupta1126@gmail.com>
2024-08-27 21:28:11 +0530

    27/08/24

--- Sliding Window/Medium/video4.cpp
+++ Sliding Window/Medium/video4.cpp
@@ -48,6 +48,30 @@ int MaxConsecutiveOnesApproach2(vector<int>a,int k){
     //S.C.-O(1)
 }
 
+int MaxConsecutiveOnesApproach3(vector<int>a,int k){
+    //OPTIMAL APPROACH
+    int n=a.size();
+    int maxLength=0;
+    int len=0;
+    int zeroes=0;
+    int l=0,r=0;
+    while(r<n){
+        if(a[r]==0) zeroes++;
+        if(zeroes>k){
+            if(a[l]==0) zeroes--;
+            l++;
+        }
+        if(zeroes<=k){
+            len=r-l+1;
+            maxLength=max(maxLength,len);
+            r++;
+        }
+    }
+    return maxLength;
+    //T.C.-O(2N)
+    //S.C.-O(1)
+}
+
 int main(){
     //MAX CONSECUTIVE ONES III
     vector<int>nums={1,1,1,0,0,0,1,1,1,1,0};
---
+++ Sliding Window/Medium/video5.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int FruitIntoBasket(vector<int>a){
+    //BRUTE FORCE
+    int n=a.size();
+    int maxLength=0;
+    set<int>s;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            s.insert(a[j]);
+            if(s.size()<=2){
+                maxLength=max(maxLength,j-i+1);
+            }
+            else{
+                break;
+            }
+        }
+        s.clear();
+    }
+    return maxLength;
+    //T.C.-O(N^2)
+    //S.C.-O(3)
+}
+
+int FruitIntoBasketApproach2(vector<int>a, int k){
+    //BETTER SOLUTION
+    int n=a.size();
+    int maxLength=0;
+    map<int,int>mpp;   //mapping-> value,frequency and remove that number while sliding whose frequency turns zero
+
+    int l=0,r=0;
+    while(r<n){
+        mpp[a[r]]++;
+        if(mpp.size()>k){
+            while(mpp.size()>k){
+                mpp[a[l]]--;
+                if(mpp[a[l]]==0){
+                    mpp.erase(a[l]);
+                }
+                l++;
+            }
+        }
+        if(mpp.size()<=k){
+            maxLength=max(maxLength,r-l+1);
+        }
+        r++;
+    }
+    return maxLength;
+    //T.C.-O(2N)
+    //S.C.-O(3)
+}
+
+int FruitIntoBasketApproach3(vector<int>a, int k){
+    //OPTIMAL SOLUTION
+    int n=a.size();
+    int maxLength=0;
+    map<int,int>mpp;   //mapping-> value,frequency and remove that number while sliding whose frequency turns zero
+
+    int l=0,r=0;
+    while(r<n){
+        mpp[a[r]]++;
+        if(mpp.size()>k){
+            mpp[a[l]]--;
+            if(mpp[a[l]]==0){
+                mpp.erase(a[l]);
+            }
+            l++;
+        }
+        if(mpp.size()<=k){
+            maxLength=max(maxLength,r-l+1);
+        }
+        r++;
+    }
+    return maxLength;
+    //T.C.-O(2N)
+    //S.C.-O(3)
+}
+
+int main(){
+    //FRUIT INTO BASKET
+    vector<int>a={3,3,3,1,2,1,1,2,3,3,4};
+    int k=2;
+    cout<<FruitIntoBasketApproach2(a,k);
+    return 0;
+}
