8fa98084693ff56f06bf0485b102bc22effd9c78
Vidit <viditgupta1126@gmail.com>
2024-09-11 20:03:49 +0530

    11/09/24

--- Heaps/Learning/video4.cpp
+++ Heaps/Learning/video4.cpp
@@ -79,11 +79,74 @@ int KthLargestElementApproach3(vector<int>a,int k){
     //S.C.-O(k)
 }
 
+int SumOfElementBtwnK1SmallestAndK2SmallestElement(vector<int>b,int k1,int k2){
+    // Using Sorting Method
+    sort(b.begin(),b.end());
+    int sum=0;
+    for(int i=k1;i<k2-1;i++){
+        sum+=b[i];
+    }
+    return sum;
+    //T.C.-O(nlog n+(k2-k1))
+    //S.C.-O(1)
+}
+
+int SumOfElementBtwnK1SmallestAndK2SmallestElementApproach2(vector<int>b,int k1,int k2){
+    //USING MAX HEAP
+    priority_queue<int> p1;
+    for(int i=0;i<k1;i++){
+        p1.push(b[i]);
+    }
+    for(int i=k1;i<b.size();i++){
+        if(b[i]<p1.top()){
+            p1.pop();
+            p1.push(b[i]);
+        }
+    }
+
+    priority_queue<int> p2;
+    k2--;
+    for(int i=0;i<k2;i++){
+        p2.push(b[i]);
+    }
+    for(int i=k2;i<b.size();i++){
+        if(b[i]<p2.top()){
+            p2.pop();
+            p2.push(b[i]);
+        }
+    }
+    int sum1=0;
+    while(!p1.empty()){
+        cout<<p1.top()<<" ";
+        sum1+=p1.top();
+        p1.pop();
+    }
+    cout<<endl;
+    int sum2=0;
+    while(!p2.empty()){
+        cout<<p2.top()<<" ";
+        sum2+=p2.top();
+        p2.pop();
+    }
+    cout<<endl;
+    return sum2-sum1;
+    //T.C.-O(nlog k1+nlog k2)
+    //S.C.-O(k1+k2)
+}
+
 int main(){
     //Kth Smallest Element
-    vector<int>a={10,3,7,4,8,9,2,6};
-    int k=4;
-    cout<<KthSmallestElementApproach3(a,k)<<endl;
-    cout<<KthLargestElementApproach3(a,k)<<endl;
+    // vector<int>a={10,3,7,4,8,9,2,6};
+    // int k=4;
+    // cout<<KthSmallestElementApproach3(a,k)<<endl;
+    // cout<<KthLargestElementApproach3(a,k)<<endl;
+
+
+    // Sum Of Element Btwn K1 Smallest And K2 Smallest Element
+    vector<int>b={20,8,22,4,12,10,14};
+    int k1=3;
+    int k2=6;
+    cout<<SumOfElementBtwnK1SmallestAndK2SmallestElementApproach2(b,k1,k2)<<endl;
+
     return 0;
 }
