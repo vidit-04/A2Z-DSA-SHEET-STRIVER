10a080b19d36178a24d163260a0b90ad1c2a720b
Vidit <viditgupta1126@gmail.com>
2024-10-06 19:30:08 +0530

    06/10/24

---
+++ Greedy Algorithm/Easy Problems/Minimum number of platforms.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int MinimumNumberOfPlatforms(vector<int>arr,vector<int>dep){
+    //BRUTE FORCE
+    //IF just find the number of intersection then we can also find number of minimum numer of platform required
+    int maxCount=0;
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        int count=0;
+        for(int j=0;j<n;j++){
+            if(arr[j]<=arr[i] && arr[i]<=dep[j]){
+                count++;
+            }
+        }
+        maxCount=max(maxCount,count);
+    }
+    return maxCount;
+    //T.C.-O(n^2)
+    //S.C.-O(1)
+}
+
+int MinimumNumberOfPlatformsApproach2(vector<int>arr,vector<int>dep){
+    //OPTIMAL APPROACH
+    sort(arr.begin(),arr.end());
+    sort(dep.begin(),dep.end());
+    int n=arr.size();
+    int i=1,j=0;
+    int count=1;
+    int maxCount=1;
+    while(i<n && j<n){
+        if(arr[i]<=dep[j]){
+            count++;
+            i++;
+        }
+        else{
+            count--;
+            j++;
+        }
+        maxCount=max(maxCount,count);
+    }
+    return maxCount;
+    //T.C.-O(nlogn)
+    //S.C.-O(1)
+}
+
+int main(){
+    vector<int>arr={900,945,955,1100,1500,1800};
+    vector<int>dep={920,1200,1130,1150,1900,2000};
+    cout<<MinimumNumberOfPlatforms(arr,dep);
+    return 0;
+}
