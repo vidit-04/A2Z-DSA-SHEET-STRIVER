ffb693cee2d37976f2452489d1f81d14476fdb0c
Vidit <viditgupta1126@gmail.com>
2024-12-19 14:36:00 +0530

    19/12/24

--- Sliding Window/Medium/video7.cpp
+++ Sliding Window/Medium/video7.cpp
@@ -2,7 +2,27 @@
 using namespace std;
 
 int NumberOfSubstringOfAllThreeCharacters(string s){
-    
+        int n = s.length();
+
+        int i=0,j=0,count=0;
+
+        unordered_map<char, int> mp;
+
+        while(j<n)
+        {
+            mp[s[j]]++;
+
+            while(mp['a']>=1 && mp['b']>=1 && mp['c']>=1)
+            {
+                count += (n - j);
+
+                //shrinking the window
+                mp[s[i]]--;
+                i++;
+            }
+            j++;
+        }
+        return count;
 }
 
 int main(){
